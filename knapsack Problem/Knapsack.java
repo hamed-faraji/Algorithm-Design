a745157b17df5ecf03e7b663bcaaa17a76628a9b
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-13 21:34:22 +0430

    Get some input from consol

---
+++ knapsack Problem/Knapsack.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Knapsack {
+	
+	public static void main(String[] args) {
+		String n, k, W, Q;
+		scanner2 = new Scanner(System.in);
+		n = scanner2.nextLine();/// first line of input is the correct answer
+		k = scanner2.nextLine();/// second line of input is data as described
+		W = scanner2.nextLine();
+		String[] Weights = n.split(" ");
+		String[] Values = k.split(" ");
+		Objects[] AllObjects = new Objects[Weights.length];
+		System.out.println("Have your inputs any Float number?(yes/no)");
+		Q = scanner2.nextLine();
+	
+	}
+}
