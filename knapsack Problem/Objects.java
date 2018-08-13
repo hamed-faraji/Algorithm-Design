1626eec36fb567b0b5a8b441e7a76cd26058698c
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-13 21:37:21 +0430

    End of Objects class

---
+++ knapsack Problem/Objects.java
@@ -0,0 +1,13 @@
+public class Objects {
+
+		int Weight;
+		int Value;
+		int ObjectNumber;
+
+		public Objects(int Weight, int Value ,int ObjectNumber) {
+			this.Weight = Weight;
+			this.Value = Value;
+			this.ObjectNumber = ObjectNumber;
+		}
+
+}
