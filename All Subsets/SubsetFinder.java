d69370df741311c5740fa4a67e577bee92ffe575
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-10 22:40:45 +0430

    Add finder method part 1

--- All Subsets/SubsetFinder.java
+++ All Subsets/SubsetFinder.java
@@ -5,6 +5,19 @@ public class SubsetFinder {
 	
 	public static int c=0; 		// c is the counter
 	
+	public static void finder(int a[],List<List<Integer>> listNum,int size){
+		if( size == 0 )
+			return;
+		List<List<Integer>> NewlistNum = new ArrayList<>();
+		for( int i = 0; i < listNum.size(); i++ ){
+			System.out.print("{ ");
+			for( int j = 0; j < listNum.get(i).size()-1; j++ )
+				System.out.print(listNum.get(i).get(j)+", ");	
+			System.out.println(listNum.get(i).get(listNum.get(i).size()-1)+" }");
+			c = c + 1;
+		}
+	}
+	
 	public static void  main(String[] args)
 	{
 		System.out.println("{ }");
