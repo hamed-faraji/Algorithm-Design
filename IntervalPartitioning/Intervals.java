30eb6ba4ce5327e875ba6feeab7e51dd802d7f37
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-11 22:37:31 +0430

    Add intervals class

---
+++ IntervalPartitioning/Intervals.java
@@ -0,0 +1,14 @@
+package intervalPartitioning;
+
+public class Intervals {
+
+	int start;
+	int end;
+	int lable;
+
+	public Intervals(int start, int end, int lable) {
+		this.end = end;
+		this.start = start;
+		this.lable = lable;
+	}
+}
