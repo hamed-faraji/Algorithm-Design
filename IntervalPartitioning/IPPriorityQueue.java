92f4029d9c0b2b31e09fe72a78ef52c17411d4d5
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-11 22:40:23 +0430

    Add Schedule to IPPriorityQueue

--- IntervalPartitioning/IPPriorityQueue.java
+++ IntervalPartitioning/IPPriorityQueue.java
@@ -5,7 +5,47 @@ import java.util.*;
 public class IPPriorityQueue {
 
 	static ArrayList<ArrayList<Intervals>> Schedule(ArrayList<Intervals> SortedIntervals) {
-	
+	final ArrayList<ArrayList<Intervals>> ClassRooms = new ArrayList<>();
+		class PriorityComparator implements Comparator<Intervals> {
+			public int compare(Intervals first, Intervals second) {
+				return first.end - second.end;
+			}
+		}
+		PriorityComparator comp = new PriorityComparator();
+		PriorityQueue<Intervals> pq = new PriorityQueue<Intervals>(SortedIntervals.size() - 1, comp);
+
+		int n = SortedIntervals.size();
+
+		int ClassRoomNum = 0;
+		for (int i = 0; i < n; i++) {
+			if (ClassRooms.size() == 0) {
+				ArrayList<Intervals> a = new ArrayList<>();
+				a.add(SortedIntervals.get(i));
+				ClassRooms.add(a);
+				SortedIntervals.get(i).lable = ClassRoomNum;
+				pq.offer(SortedIntervals.get(i));
+				ClassRoomNum++;
+				// System.out.println("+");
+			} else {
+				// System.out.println(pq.poll().start);
+				if (SortedIntervals.get(i).start < pq.peek().end) {
+					ArrayList<Intervals> a = new ArrayList<>();
+					a.add(SortedIntervals.get(i));
+					ClassRooms.add(a);
+					SortedIntervals.get(i).lable = ClassRoomNum;
+					pq.offer(SortedIntervals.get(i));
+					ClassRoomNum++;
+					// System.out.println("-");
+				} else {
+					int temp = pq.poll().lable;
+					ClassRooms.get(temp).add(SortedIntervals.get(i));
+					SortedIntervals.get(i).lable = temp;
+					pq.offer(SortedIntervals.get(i));
+				}
+			}
+		}
+
+		return ClassRooms;
 	}
 
 	public static void main(String[] args) {
