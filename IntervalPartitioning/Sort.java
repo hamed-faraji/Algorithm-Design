c7886928bc442e74d826a3566c4cd286fade311c
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-12 23:40:58 +0430

    Add constructor Sort

--- IntervalPartitioning/Sort.java
+++ IntervalPartitioning/Sort.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 
 public class Sort {
 
+	static void Sort(final ArrayList<Intervals> SortedIntervals) {
+		int n = SortedIntervals.size();
+		if (n < 2)
+			return;
+		int mid = n / 2;
+		ArrayList<Intervals> left = new ArrayList<>();
+		ArrayList<Intervals> right = new ArrayList<>();
+		for (int i = 0; i < mid; i++)
+			left.add(SortedIntervals.get(i));
+		for (int i = mid; i < n; i++)
+			right.add(SortedIntervals.get(i));
+		Sort(left);
+		Sort(right);
+		merge(SortedIntervals, left, right);
+	}
 	
 
 	
